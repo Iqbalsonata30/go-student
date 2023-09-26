e92163516a6c5be559ce99d9ae766d73b3b86169
Iqbal Sonata <iqbalsonata2@gmail.com>
2023-09-26 21:04:56 +0700

    create repository find all students

--- repository/student_repository.go
+++ repository/student_repository.go
@@ -10,7 +10,7 @@ import (
 type StudentRepository interface {
 	Save(context.Context, *sql.Tx, domain.Student) (*domain.Student, error)
 	// FindById(context.Context, *sql.Tx, string) (*domain.Student, error)
-	// FindAll(context.Context, *sql.Tx) (*[]domain.Student, error)
+	FindAll(context.Context, *sql.Tx) (*[]domain.Student, error)
 	// Update(context.Context, *sql.Tx, string) (*domain.Student, error)
 	// Delete(context.Context, *sql.Tx, string) error
 }
